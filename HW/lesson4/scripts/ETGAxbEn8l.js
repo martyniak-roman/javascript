acb1f41268eaf4492817badb2075dc57dc5c8e36
martyniak-roman <roman.martyniakdev@gmail.com>
2026-04-11 18:25:23 +0000

    complete some tasks lesson 4

---
+++ HW/lesson4/scripts/ETGAxbEn8l.js
@@ -0,0 +1,5 @@
+function roundSquare(radius){
+    return Math.PI * radius * radius;
+}
+
+roundSquare(5)
---
+++ HW/lesson4/scripts/I2XsG6f.js
@@ -0,0 +1,6 @@
+function area(a,b) {
+    const s = a * b;
+    return s;
+}
+
+console.log(area(5,3))
